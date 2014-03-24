d94221a9b405e5b7a122bfb55945095ba71953ae
Nathan Kleekamp <nkleekamp@gmail.com>
2014-03-24 15:44:49 -0400

    working, but still needs keyboard controlls and to complete documentation

--- src/jquery.inject-video.js
+++ src/jquery.inject-video.js
@@ -1,29 +1,22 @@
-// the semi-colon before function invocation is a safety net against concatenated
-// scripts and/or other plugins which may not be closed properly.
 ;(function ( $, window, document, undefined ) {
 
-		// undefined is used here as the undefined global variable in ECMAScript 3 is
-		// mutable (ie. it can be changed by someone else). undefined isn't really being
-		// passed in so we can ensure the value of it is truly undefined. In ES5, undefined
-		// can no longer be modified.
-
-		// window and document are passed through as local variable rather than global
-		// as this (slightly) quickens the resolution process and can be more efficiently
-		// minified (especially when both are regularly referenced in your plugin).
-
 		// Create the defaults once
-		var pluginName = "defaultPluginName",
+		var pluginName = "injectVideo",
+
+        // thumbQuality options: 'default.jpg', 'sddefault.jpg', 'mqdefault.jpg', 'hqdefault.jpg',
+        // 'maxresdefault.jpg'
+
+        // parameters options: Any of the YouTube player parameters from
+        // https://developers.google.com/youtube/player_parameters#Parameters
+
 				defaults = {
-				propertyName: "value"
+            thumbQuality: 'mqdefault.jpg',
+            parameters: {}
 		};
 
 		// The actual plugin constructor
 		function Plugin ( element, options ) {
 				this.element = element;
-				// jQuery has an extend method which merges the contents of two or
-				// more objects, storing the result in the first object. The first object
-				// is generally empty as we don't want to alter the default options for
-				// future instances of the plugin
 				this.settings = $.extend( {}, defaults, options );
 				this._defaults = defaults;
 				this._name = pluginName;
@@ -32,17 +25,54 @@
 
 		Plugin.prototype = {
 				init: function () {
-						// Place initialization logic here
-						// You already have access to the DOM element and
-						// the options via the instance, e.g. this.element
-						// and this.settings
-						// you can add more functions like the one below and
-						// call them like so: this.yourOtherFunction(this.element, this.settings).
-						console.log("xD");
+            this.$videos = $(this.element);
+            this.loadPoster();
+            this.bindUiActions();
 				},
-				yourOtherFunction: function () {
-						// some logic
-				}
+
+        bindUiActions: function () {
+            this.click();
+        },
+
+        click: function () {
+            var _this = this;
+
+            _this.$videos.
+                on('click', function () {
+                    _this.loadVideo( $(this) );
+            });
+        },
+
+        loadPoster: function () {
+            var _this = this;
+
+            _this.$videos.each(function (i) {
+                var baseUrl = 'http://img.youtube.com/vi/',
+                    videoId = $(this).attr('data'),
+                    url = baseUrl + videoId + '/' + _this.settings.thumbQuality,
+                    code = $('<img>', {
+                        'src': url
+                    });
+
+                $(this).prepend(code);
+            });
+        },
+
+        loadVideo: function (i) {
+            var _this = i,
+                baseUrl = 'https://www.youtube.com/embed/',
+                videoId = _this.attr('data'),
+                params = $.param(this.settings.parameters),
+                url = baseUrl + videoId + '?' + params,
+                code = $('<iframe />', {
+                    frameborder: '0',
+                    allowfullscreen: 'true',
+                    src: url
+                });
+
+            _this.children().remove();
+            _this.append(code);
+        }
 		};
 
 		// A really lightweight plugin wrapper around the constructor,
